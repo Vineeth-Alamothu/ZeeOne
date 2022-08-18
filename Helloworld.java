2353ba4cf2832b71313d6f1f4a04588416a927db
Vineeth-Alamothu <110161466+Vineeth-Alamothu@users.noreply.github.com>
2022-08-18 14:11:22 +0530

    new file added

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+class Helloworld {
+    public static void main(String[] args){
+        Scanner SC = new Scanner(System.in);
+        System.out.println("enter name");
+        String name = SC.nextLine();
+        System.out.println("the name entered is "+name);
+        
+    }
+}
